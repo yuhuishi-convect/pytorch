bbdd4787bc6f4ecfdd599f4ec3514920b6457e9b
Yu, Guangye <guangye.yu@intel.com>
2025-08-28 21:50:14 +0000

    "[Don't Review] Test XPU CI"
    This reverts commit 0df78f0c11c1aafe0a3c5a88cca8565a81f03a6b.
    
    ghstack-source-id: 080609597e2a8a8634d9d1c7c5c061753b9d9b17
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/159718
    
    Revert "[inductor] respect layout tags for ops with registered lowerings (#159134)"
    
    This reverts commit 669009bcd1ee44981ab5c4524b7b6a098e991985.

--- torch/csrc/xpu/Module.cpp
+++ torch/csrc/xpu/Module.cpp
@@ -13,6 +13,12 @@
 
 using namespace torch;
 
+// Wrapper class for XPU UUID
+struct XPUuuid {
+  explicit XPUuuid(const std::array<unsigned char, 16>& uuid) : bytes(uuid) {}
+  const std::array<unsigned char, 16>& bytes;
+};
+
 // XPU management methods
 
 static PyObject* THXPModule_getArchFlags(PyObject* self, PyObject* noargs) {
@@ -295,7 +301,19 @@ static void registerXpuDeviceProperties(PyObject* module) {
     return static_cast<int64_t>(prop.architecture);
   };
 #endif
+  auto get_xpu_uuid = [](const DeviceProp& prop) { return XPUuuid(prop.uuid); };
+
   auto m = py::handle(module).cast<py::module>();
+
+  py::class_<XPUuuid>(m, "_XPUuuid")
+      .def_property_readonly(
+          "bytes",
+          [](const XPUuuid& uuid) {
+            return std::vector<uint8_t>(uuid.bytes.begin(), uuid.bytes.end());
+          })
+      .def("__str__", [](const XPUuuid& uuid) {
+        return uuid_to_string(reinterpret_cast<const char*>(uuid.bytes.data()));
+      });
 
 #define DEFINE_READONLY_MEMBER(member) \
   def_readonly(#member, &DeviceProp::member)
@@ -328,6 +346,7 @@ static void registerXpuDeviceProperties(PyObject* module) {
       .def_property_readonly("architecture", get_device_architecture)
 #endif
       .def_property_readonly("type", get_device_type)
+      .def_property_readonly("uuid", get_xpu_uuid)
       .def(
           "__repr__",
           [&get_device_type, &gpu_subslice_count](const DeviceProp& prop) {
@@ -335,7 +354,9 @@ static void registerXpuDeviceProperties(PyObject* module) {
             stream << "_XpuDeviceProperties(name='" << prop.name
                    << "', platform_name='" << prop.platform_name << "', type='"
                    << get_device_type(prop) << "', device_id=0x" << std::hex
-                   << std::uppercase << prop.device_id << std::dec
+                   << std::uppercase << prop.device_id << std::dec << ", uuid="
+                   << uuid_to_string(
+                          reinterpret_cast<const char*>(prop.uuid.data()))
                    << ", driver_version='" << prop.driver_version
                    << "', total_memory="
                    << prop.global_mem_size / (1024ull * 1024) << "MB"
